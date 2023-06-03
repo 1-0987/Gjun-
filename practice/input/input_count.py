949a08181e48f649c2b7fa5a469e8aa622034249
1-0987 <wagonwu@gmail.com>
2023-06-03 20:29:42 +0800

    Second commit

--- practice/input/input_count.py
+++ practice/input/input_count.py
@@ -1,4 +1,3 @@
 text = "蕭志豪 許軒豪 蕭志豪 許軒豪 蕭志豪 蕭志豪" #計算手術量
 find = input("How many surgery were performed by the chosen doctor?(please enter the doctor) ")
 print(text.count(find)) # 用count()方法來找find變數的值在text字串裡出現幾次
-print("我好黑")
